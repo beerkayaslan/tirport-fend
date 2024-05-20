35bb3b8bc0da3c146f269a4710d89820d08d7dac
Berkay Aslan <berkayaslan00@gmail.com>
2024-05-20 23:58:19 +0300

    add pages

---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center px-4">
+      <h3>Yükleniyor...</h3>
+    </div>
+  );
+}
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,3 +1,45 @@
+import { lazy } from "react";
+import { useSelector } from "react-redux";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import { FallbackProvider } from "@/components/Fallback/FallbackProvider";
+import Loader from "@/components/Loader";
+import DashboardLayout from "@/layouts/DashboardLayout";
+import { ROUTES } from "@/router/url";
+import { RootState } from "@/store/index";
+import { AuthStatusEnum } from "@/types/auth/type";
+
+const Index = lazy(() => import("@/pages/index/Index"));
+const Login = lazy(() => import("@/pages/auth/login/Index"));
+
 export default function Router() {
-  return "test";
+  const AUTH_STATUS = useSelector(
+    (state: RootState) => state.auth.AUTH_STATUS,
+  ) as AuthStatusEnum;
+
+  if (AUTH_STATUS === AuthStatusEnum.LOADING) {
+    return <Loader />;
+  }
+
+  if (AUTH_STATUS === AuthStatusEnum.LOGGED_IN) {
+    return (
+      <DashboardLayout>
+        <FallbackProvider>
+          <Routes>
+            <Route path={ROUTES.INDEX.PATH} Component={Index} />
+            <Route path="*" element={<Navigate to={ROUTES.INDEX.PATH} />} />
+          </Routes>
+        </FallbackProvider>
+      </DashboardLayout>
+    );
+  }
+
+  return (
+    <FallbackProvider>
+      <Routes>
+        <Route path={ROUTES.LOGIN.PATH} Component={Login} />
+        <Route path="*" element={<Navigate to={ROUTES.LOGIN.PATH} />} />
+      </Routes>
+    </FallbackProvider>
+  );
 }
