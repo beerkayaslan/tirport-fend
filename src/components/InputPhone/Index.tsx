14ede3ded948f659783eee02bb3104f7b19ae9df
Berkay Aslan <46554154+beerkayaslan@users.noreply.github.com>
2024-05-24 13:59:07 +0300

    bug fix

--- src/components/InputPhone/Index.tsx
+++ src/components/InputPhone/Index.tsx
@@ -18,6 +18,7 @@ export default function Index({ ...props }: FormItemProps) {
         disableSearchIcon
         searchPlaceholder="Ara"
         containerClass="custom-phone-input"
+        searchClass="phone-search-box"
         inputProps={{
           required: props.required
         }}
